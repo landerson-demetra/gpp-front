f6242adc3e1a261ad3f63341d4e9841393d83010
Landersson <landerson.araujo@demetrati.com.br>
2018-08-03 16:20:38 -0300

    Começando o front end do GPP

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+
+import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld2 from './components/HelloWorld2.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        { path: '/', component: HelloWorld },
+        { path: '/2', component: HelloWorld2 }
+    ]
+})
+
 new Vue({
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
