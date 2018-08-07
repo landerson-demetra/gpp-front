ea41676c6fbe2560325a7d968dd3f28ecdbe9137
Landersson <landerson.araujo@demetrati.com.br>
2018-08-07 17:58:32 -0300

    Changes

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,10 +1,16 @@
 import Dashboard from '@/components/Admin/Dashboard'
+import GestaoPatrimonios from '@/components/Admin/GestaoPatrimonios'
 
 var routes = [
     {
         path: 'dashboard',
         component: Dashboard,
         name: 'Dashboard'
+    },
+    {
+        path: 'gestao-patrimonios',
+        component: GestaoPatrimonios,
+        name: 'GestaoPatromonios'
     }
 ];
 
